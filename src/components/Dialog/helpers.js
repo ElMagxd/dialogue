5d605bf3a639d27c5be49e7929d0e5bb101a8288
Vitalii Hrankin <vitaliihr@wizardsdev.com>
2022-04-10 14:39:20 +0300

    fixed normalizeDialog logic bug

--- src/components/Dialog/helpers.js
+++ src/components/Dialog/helpers.js
@@ -1,52 +1,49 @@
 import dayjs from "dayjs";
 
 export const normalizeDialog = dialog => {
-	const newDialog = [];
+  const newDialog = [];
 
-	dialog.forEach((item, index) => {
-		if (index === 0 || dialog[index - 1]) {
-			const first = dayjs((index === 0 ? item.date : dialog[index - 1]).date)
-			const diff = first.diff(item.date, 'day');
+  dialog.forEach((item, i) => {
+	const first = dayjs((i === 0 ? item : dialog[i - 1]).date);
+	const diff = first.diff(item.date, "day");
 
-			if (index === 0 || diff) {
+	if (i === 0 || diff) {
+        newDialog.push({
+			type: "title",
+			id: `item-title-${item.id}`,
+			date: item.date,
+        });
+	}
 
-				newDialog.push({
-					type: 'title',
-					id: `item-title-${item.id}`,
-					date: item.date,
-				});
-			}
-		}
+    if (i === 0 || item.is !== dialog[i - 1].is || diff) {
+      newDialog.push({
+        type: "message",
+        id: `item-message-${item.id}`,
+        avatar: item.avatar,
+        isReverse: item.is === "my",
+        messages: [
+          {
+            text: item.message,
+            status: item.status,
+            id: item.id,
+            date: item.date,
+          },
+        ],
+      });
+    } else {
+		const position = newDialog.length - 1;
+		const currentItem = newDialog[position];
 
-		if (index === 0 || item.is !== dialog[index - 1].is) {
-			newDialog.push({
-				type: 'message',
-				id: `item-message-${item.id}`,
-				avatar: item.avatar,
-				isReverse: item.is === "my",
-				messages: [
-					{
-						text: item.message,
-						status: item.status,
-						id: item.id,
-						date: item.date,
-					}
-				]
-			});
-		} else {
-			const position = newDialog.length - 1;
-
-			newDialog[position] = {
-				...newDialog[position],
-				messages: newDialog[position].messages.concat({
-					text: item.message,
-					status: item.status,
-					id: item.id,
-					date: item.date,
-				}),
-			}
-		}
-	});
-
-	return newDialog;
-}
+		newDialog[position] = {
+			...currentItem,
+			messages: currentItem.messages.concat({
+				text: item.message,
+				status: item.status,
+				id: item.id,
+				date: item.date,
+			}),
+		};
+    }
+  });
+  return newDialog;
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import WebFontLoader from "webfontloader";
 import App from './App';
 
@@ -9,9 +9,6 @@ WebFontLoader.load({
   },
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
